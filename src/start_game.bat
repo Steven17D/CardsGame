04d53a6a34976bc70ee9b2411e7df8ac5855e018
Steven Dashevsky <stiven1999d@gmail.com>
2017-11-12 00:38:08 +0200

    added executable

--- src/start_game.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "python CardsGame\Server.pyc & exit"
-start cmd.exe /k "CardsGame\bin\Debug\CardsGame.exe & exit"
---
+++ start.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "python src\CardsGame\Server.pyc & exit"
+start cmd.exe /k "dist\CardsGame.exe & exit"
+start cmd.exe /k "dist\CardsGame.exe & exit"
